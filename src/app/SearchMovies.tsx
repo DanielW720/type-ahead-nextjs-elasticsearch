55bec40128a871983729712f878db1e7eb5aeb45
Daniel Westerlund <62758767+DanielW720@users.noreply.github.com>
2023-06-23 11:29:52 +0200

    Update SearchMovies.tsx CSS

--- src/app/SearchMovies.tsx
+++ src/app/SearchMovies.tsx
@@ -12,7 +12,7 @@ export const SearchMovies = () => {
       <FetchMoviesButton setMovies={setMovies} />
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id} className="px-6 py-2 text-white">
+          <li key={movie.id} className="border-[2px] border-blue-400 bg-pink-200 px-6 py-2 text-white">
             <button
               onClick={() => {
                 window.open(`https://google.com/search?q=${movie.title} movie`);
